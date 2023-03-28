9e9ee3efd80a3755f802a26ce34a7fcd7f5ed578
emohex <olumidemm@gmail.com>
2023-03-28 11:37:32 +0100

    config again

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-  target: 'static',
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
